9862f94d2fac82272fc0631b104192ef63852d66
Menezess42 <arielmenezess@hotmail.com>
2026-07-22 16:01:33 -0300

    first commit

---
+++ devenv.nix
@@ -0,0 +1,49 @@
+{ pkgs, lib, config, inputs, ... }:
+{
+    packages = [
+        pkgs.pyright
+    ];
+    languages.python = {
+        enable = true;
+        package = pkgs.python313.withPackages (p: with p; [
+            kagglehub
+            numpy
+            scipy
+            pandas
+            matplotlib
+            pytest
+            ipykernel
+            ipython
+            kaggle
+            plotly
+            nbformat
+            pyqt5
+            scikit-learn
+            pip
+
+            #NVIM
+            jedi
+            jedi-language-server
+            black
+            flake8
+            sentinel
+            python-lsp-server
+            # END NVIM
+
+            virtualenv
+            pyflakes
+            isort
+            debugpy
+            nltk
+        ]);
+
+        venv.enable = true;
+        venv.requirements = ''
+            apyori==1.1.2
+            tensorflow
+        '';
+    };
+    enterShell = ''
+      echo "$(python --version) — venv ativo"
+    '';
+}
