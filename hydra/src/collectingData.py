aa61345e9ac9f3ccc754cd79072089bce57f9561
Menezess42 <arielmenezess@hotmail.com>
2026-07-25 05:36:18 -0300

    successfully access my notion main page
    TODO: retrieve info from my DB

--- hydra/src/collectingData.py
+++ hydra/src/collectingData.py
@@ -1,4 +1,23 @@
+from dotenv import load_dotenv, dotenv_values
+import requests
+
+# geting keys
+KEYS = dotenv_values('.env')
+
+
 # Notion
+NOTION_VERSION = "2026-03-11"
+NOTION_API_URL = "https://api.notion.com/v1/blocks/"
+
+url = f"{NOTION_API_URL}{KEYS['NOTION_MAIN']}/children"
+
+headers = {
+    "Notion-Version": f"{NOTION_VERSION}",
+    "Authorization": f"Bearer {KEYS['NOTION_KEY']}"
+}
+response = requests.get(url, headers=headers)
+
+print(response.text)
 
 # Obsidian
 ## test if is running
