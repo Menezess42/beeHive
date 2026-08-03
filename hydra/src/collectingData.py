f4dd53d3216a7ce3a4dcc7cadac90ed2a0b55f57
Menezess42 <arielmenezess@hotmail.com>
2026-08-03 14:57:47 -0300

    testing notion API

--- hydra/src/collectingData.py
+++ hydra/src/collectingData.py
@@ -1,23 +1,41 @@
-from dotenv import load_dotenv, dotenv_values
 import requests
+from dotenv import dotenv_values, load_dotenv
 
 # geting keys
-KEYS = dotenv_values('.env')
+KEYS = dotenv_values(".env")
 
 
 # Notion
 NOTION_VERSION = "2026-03-11"
-NOTION_API_URL = "https://api.notion.com/v1/blocks/"
+NOTION_API_URL = "https://api.notion.com/v1/"
 
-url = f"{NOTION_API_URL}{KEYS['NOTION_MAIN']}/children"
+flags = {"users": "users"}
+
+url = f"{NOTION_API_URL}/{flags['users']}/{KEYS['NOTION_LIBRARY']}/query"
+
+payload = {"page_size": 100}
 
 headers = {
     "Notion-Version": f"{NOTION_VERSION}",
-    "Authorization": f"Bearer {KEYS['NOTION_KEY']}"
+    "Authorization": f"Bearer {KEYS['NOTION_KEY']}",
 }
-response = requests.get(url, headers=headers)
 
-print(response.text)
+response = requests.post(url, json=payload, headers=headers)
+
+url = f"https://api.notion.com/v1/data_sources/{KEYS['NOTION_LIBRARY']}/query"
+
+payload = {
+    "page_size": 123,
+    "Notion-Version": f"{NOTION_VERSION}",
+    "Authorization": f"Bearer {KEYS['NOTION_TOKEN']}",
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+from pprint import pprint
+
+a = response.json()
+pprint(a)
 
 # Obsidian
 ## test if is running
