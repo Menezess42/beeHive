14c0c94c5df4e3fcdd98c258c020a7a4b47c017a
Menezess42 <arielmenezess@hotmail.com>
2026-09-07 12:39:51 -0300

    refact: removing old code

--- beeHive/src/server/bh_daemon.py
+++
@@ -1,5 +0,0 @@
-# Dev aspects
-# # Bee able to connect
-# # Bee able to observe when the server is running (print "listening...")
-# # Bee able to listen to terminal requests
-# # # Respond to terminal requests
